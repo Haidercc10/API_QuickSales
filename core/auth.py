3418d623ba7dd554042884d31667a6f57e424c24
Háider Cantillo Colón <73559575+Haidercc10@users.noreply.github.com>
2025-12-19 14:08:29 -0500

    Generación de token

--- core/auth.py
+++ core/auth.py
@@ -1,15 +1,38 @@
-from fastapi import Depends, HTTPException
-from fastapi.security import OAuth2PasswordBearer
-from jose import jwt, JWTError
-from core.config import SECRET_KEY, ALGORITHM
+from datetime import datetime, timedelta
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer, HTTPBearer, HTTPAuthorizationCredentials
+from jose import jwt, JWTError
+from core.config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+
+security = HTTPBearer()
 
 def create_token(data : dict):
-    return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+    to_encode = data.copy()
 
-def decode_token(token: str = Depends(oauth2_scheme)):
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({
+        "expire": int(expire.timestamp()),
+        "iat": int(datetime.utcnow().timestamp())
+    })
+
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_token(
+    credentials: HTTPAuthorizationCredentials = Depends(security)
+):
     try:
-        return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        token = credentials.credentials
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
     except JWTError:
         raise HTTPException(status_code=401, detail="Invalid token")
+
+def get_current_user(payload: dict = Depends(decode_token)):
+    user_id = payload.get("sub")
+
+    if not user_id:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    return user_id
+
--- routers/login.py
+++ routers/login.py
@@ -4,7 +4,7 @@ from typing import Annotated
 from fastapi .exceptions import HTTPException
 from jose import jwt
 from sqlalchemy.orm import Session
-from core.auth import create_token, decode_token
+from core.auth import create_token, decode_token, get_current_user
 from core.config_bd import get_db
 from models.login import Login
 from models.usuario import Usuario
@@ -23,11 +23,18 @@ def login(form: Login):
     user = users.get(form.Usu_Id)
 
     if not user or user["Usu_Password"] != form.Usu_Password:
-        raise HTTPException(status_code=404, detail="Usuario o contraseña incorrectos")
+        raise HTTPException(status_code=401, detail="Usuario o contraseña incorrectos")
 
-    token = create_token({ "Usu_Id": user["Usu_Id"] })
+    token = create_token({ "sub": str(user["Usu_Id"]) })
 
     return {
         "access_token": token,
         "token_type": "bearer"
+    }
+
+@router.get("/profile")
+def profile(user_id: str = Depends(get_current_user)):
+    return {
+        "message": "Ruta protegida",
+        "user_id": user_id
     }
