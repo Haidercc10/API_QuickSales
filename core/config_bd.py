36859b4ba36a700b51ffcfbecf1931435ca609c7
Háider Cantillo Colón <73559575+Haidercc10@users.noreply.github.com>
2025-12-23 14:08:42 -0500

    Cambios
    Adición de cambios para conectar bd con login

--- core/config_bd.py
+++ core/config_bd.py
@@ -1,12 +1,19 @@
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+load_dotenv(".env")
+
 DATABASE_URL = "mysql+mysqlconnector://root:Ha061096**@localhost/BD_QuickSales"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread" : False})
+engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
 
